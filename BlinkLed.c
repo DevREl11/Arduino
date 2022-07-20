8294d9fe6a9823314b81a250c825024ba75315cb
The Cortex Hub <107203155+DevREl11@users.noreply.github.com>
2022-07-20 04:28:07 +0200

    Create BlinkLed.c

---
+++ BlinkLed.c
@@ -0,0 +1,16 @@
+int redLed = 12;
+
+void setup(){
+pinMode(redLed, OUTPUT);
+}
+void loop(){
+
+  digitalWrite(redLed, HIGH);
+  delay(1000);
+  digitalWrite(redLed, LOW);
+  delay(1000);
+  digitalWrite(redLed, HIGH);
+  delay(1000);
+  digitalWrite(redLed, LOW);
+  
+}
